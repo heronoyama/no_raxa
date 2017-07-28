63ad5b46dd44c9a0e6bf4e81b769e60366d94d9f
Heron Oyama <heroyama2@gmail.com>
2017-07-28 14:12:23 -0300

    Refactoring

--- cakephp/src/Controller/AppController.php
+++ cakephp/src/Controller/AppController.php
@@ -1,44 +1,14 @@
 <?php
-/**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link      http://cakephp.org CakePHP(tm) Project
- * @since     0.2.9
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
- */
+
 namespace App\Controller;
 
 use Cake\Controller\Controller;
 use Cake\Event\Event;
 
-/**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @link http://book.cakephp.org/3.0/en/controllers.html#the-app-controller
- */
-class AppController extends Controller
-{
+class AppController extends Controller {
 
-    /**
-     * Initialization hook method.
-     *
-     * Use this method to add common initialization code like loading components.
-     *
-     * e.g. `$this->loadComponent('Security');`
-     *
-     * @return void
-     */
-    public function initialize()
-    {
+    
+    public function initialize() {
         parent::initialize();
 
         $this->loadComponent('RequestHandler');
@@ -52,14 +22,8 @@ class AppController extends Controller
         //$this->loadComponent('Csrf');
     }
 
-    /**
-     * Before render callback.
-     *
-     * @param \Cake\Event\Event $event The beforeRender event.
-     * @return \Cake\Network\Response|null|void
-     */
-    public function beforeRender(Event $event)
-    {
+    
+    public function beforeRender(Event $event) {
         if (!array_key_exists('_serialize', $this->viewVars) &&
             in_array($this->response->type(), ['application/json', 'application/xml'])
         ) {
@@ -72,7 +36,44 @@ class AppController extends Controller
             //The best i could manage
             return json_decode($this->request->getData()[0],true);
         
-        
         return $this->request->getData();
+    }
+
+    protected function save($model,$destiny){
+        $model = $this->controller()->patchEntity($model, $this->getParsedData());
+        if ($this->controller()->save($model)) {
+            if(!$this->request->is('json')){
+                $this->flashSucess();
+                return $this->redirect($destiny);
+            }
+        }
+
+        if(!$this->request->is('json'))
+            $this->flashError();
+    }
+
+    protected function deleteModel($model,$destiny){
+        $sucess = $this->controller()->delete($model);
+        if ($this->request->is('json'))
+            return;
+        
+        if($sucess)    
+            $this->flashSucess();
+        else
+            $this->flashError();
+        
+        return $this->redirect($destiny);
+    }
+
+    private function flashSucess(){
+        $this->Flash->success(__('Operação realizada com sucesso'));
+    }
+
+    private function flashError(){
+        $this->Flash->success(__("Oops, algo deu errado. Por favor, tente novamente."));   
+    }
+
+    protected function controller(){
+        return null;
     }
 }
--- cakephp/src/Controller/EventosController.php
+++ cakephp/src/Controller/EventosController.php
@@ -3,8 +3,7 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 
-class EventosController extends AppController
-{
+class EventosController extends AppController {
 
     public function index() {
         $eventos = $this->paginate($this->Eventos);
@@ -25,19 +24,8 @@ class EventosController extends AppController
     public function add() {
         $evento = $this->Eventos->newEntity();
         if ($this->request->is('post')) {
-            // print_r($this->getParsedData());
-            $evento = $this->Eventos->patchEntity($evento, $this->getParsedData());
-            if ($this->Eventos->save($evento)) {
-                if(!$this->request->is('json')){
-                    $this->Flash->success(__('The evento has been saved.'));
-
-                    return $this->redirect(['action' => 'index']);
-                }
-            }
-            if(!$this->request->is('json'))
-                $this->Flash->error(__('The evento could not be saved. Please, try again.'));
+            $this->save($evento,['action' => 'index']);
         }
-
         $this->set(compact('evento'));
         $this->set('_serialize', ['evento']);
     }
@@ -47,17 +35,7 @@ class EventosController extends AppController
             'contain' => []
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
-
-            $evento = $this->Eventos->patchEntity($evento, $this->getParsedData());
-            if ($this->Eventos->save($evento)) {
-                if(!$this->request->is('json')){
-                    $this->Flash->success(__('The evento has been saved.'));
-
-                    return $this->redirect(['action' => 'index']);
-                }
-            }
-            if(!$this->request->is('json'))
-                $this->Flash->error(__('The evento could not be saved. Please, try again.'));
+            $this->save($evento,['action' => 'index']);
         }
         $this->set(compact('evento'));
         $this->set('_serialize', ['evento']);
@@ -66,14 +44,10 @@ class EventosController extends AppController
     public function delete($id = null) {
         $this->request->allowMethod(['post', 'delete']);
         $evento = $this->Eventos->get($id);
-        if ($this->Eventos->delete($evento)) {
-            if(!$this->request->is('json'))
-                $this->Flash->success(__('The evento has been deleted.'));
-        } else {
-            if(!$this->request->is('json'))
-                $this->Flash->error(__('The evento could not be deleted. Please, try again.'));
-        }
-        if(!$this->request->is('json'))
-            return $this->redirect(['action' => 'index']);
+        $this->deleteModel($evento,['action' => 'index']);
+    }
+
+    protected function controller(){
+        return $this->Eventos;
     }
 }
